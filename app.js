2c7e2f3e79dfcbd57421774ac509835eb9484018
A <alzemamia@gmail.com>
2024-07-15 01:31:35 +0300

    backend setup done

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const notFound = require("./middlewares/notFoundHandler");
+const errorHandler = require("./middlewares/errorHandler");
+const connectDB = require("./database");
+const passport = require("passport");
+const { localStrategy, jwtStrategy } = require("./middlewares/passport");
+const path = require("path");
+const morgan = require("morgan");
+const cors = require("cors");
+
+const app = express();
+
+connectDB();
+
+// middlewares before routers
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(cors());
+app.use(passport.initialize());
+passport.use("local", localStrategy);
+passport.use("jwt", jwtStrategy);
+app.use("/media", express.static(path.join(__dirname, "media")));
+
+//  routers
+
+// middlewares after routers
+
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(8000, () => {
+  console.log("The application is running on localhost:8000");
+});
