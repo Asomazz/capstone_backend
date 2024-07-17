1f0b6990087ecd797003480a4ae9c03c1b10899d
abdulrahman <abdulrahmanahmad_979@outlook.com>
2024-07-17 18:19:30 +0300

    product

---
+++ apis/product/controllers.js
@@ -0,0 +1,34 @@
+const Product = require("../../models/Product.js");
+
+const createOneProduct = async (req, res, next) => {
+  try {
+    req.body.user = req.user._id;
+    if (req.file) {
+      req.body.image = req.file.path;
+    }
+
+    // const product = {
+    //   title: req.body.title,
+    //   description: req.body.description,
+    //   image: req.body.image,
+    //   price: req.body.price,
+
+    // };
+
+    const newProduct = await Product.create(req.body);
+    // Update the creator with the products
+    await Creator.findOneAndUpdate(
+      { _id: req.body.creatorId },
+      {
+        $push: { products: newProduct._id },
+      }
+    );
+    return res.status(201).json(newProduct);
+  } catch (error) {
+    return next(error);
+  }
+};
+
+module.exports = {
+  createOneProduct,
+};
